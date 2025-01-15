f9b8182d5eb47577ca6e28c133de964ebfc3defd
EC2 Default User <ec2-user@ip-172-31-27-161.ec2.internal>
2025-01-15 13:35:31 +0000

    Move untracked changes to new branch

--- backend/cmd/main.go
+++ backend/cmd/main.go
@@ -1,35 +1,35 @@
 package main
 
 import (
-	"Learning-Mode-AI/pkg/config"
-	"Learning-Mode-AI/pkg/router"
-	"Learning-Mode-AI/pkg/services"
-	"log"
-	"net/http"
+        "Learning-Mode-AI/pkg/config"
+        "Learning-Mode-AI/pkg/router"
+        "Learning-Mode-AI/pkg/services"
+        "log"
+        "net/http"
 
-	"github.com/gorilla/handlers"
-	"github.com/joho/godotenv"
+        "github.com/gorilla/handlers"
+        "github.com/joho/godotenv"
 )
 
 func init() {
-	err := godotenv.Load(".env")
-	if err != nil {
-		log.Fatal("Error loading .env file")
-	}
-	config.InitConfig()
-	services.InitRedis()
+        err := godotenv.Load(".env")
+        if err != nil {
+                log.Fatal("Error loading .env file")
+        }
+        config.InitConfig()
+        services.InitRedis()
 }
 
 func main() {
-	r := router.NewRouter() // Initialize your router
+        r := router.NewRouter() // Initialize your router
 
-	// Set CORS options
-	headersOk := handlers.AllowedHeaders([]string{"X-Requested-With", "Content-Type", "Authorization"})
-	originsOk := handlers.AllowedOrigins([]string{"https://www.youtube.com"})
-	methodsOk := handlers.AllowedMethods([]string{"GET", "HEAD", "POST", "PUT", "OPTIONS"})
+        // Set CORS options
+        headersOk := handlers.AllowedHeaders([]string{"X-Requested-With", "Content-Type", "Authorization"})
+        originsOk := handlers.AllowedOrigins([]string{"*"})
+        methodsOk := handlers.AllowedMethods([]string{"GET", "HEAD", "POST", "PUT", "OPTIONS"})
 
-	log.Println("Starting server on :8080...")
-	if err := http.ListenAndServe(":8080", handlers.CORS(originsOk, headersOk, methodsOk)(r)); err != nil {
-		log.Fatalf("Could not start server: %s\n", err.Error())
-	}
+        log.Println("Starting server on :8080...")
+        if err := http.ListenAndServe(":8080", handlers.CORS(originsOk, headersOk, methodsOk)(r)); err != nil {
+                log.Fatalf("Could not start server: %s\n", err.Error())
+        }
 }
