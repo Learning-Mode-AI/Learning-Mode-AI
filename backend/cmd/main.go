9cdff97773e672f10a49e24b4a2e2288ce164914
Uday Kanwar <udaykanwar007@gmail.com>
2025-03-19 18:30:39 -0400

    Removed unecessary comments

--- backend/cmd/main.go
+++ backend/cmd/main.go
@@ -37,5 +37,5 @@ func main() {
 	if err := http.ListenAndServe(":8080", handlers.CORS(originsOk, headersOk, methodsOk)(r)); err != nil {
 		log.Fatalf("Could not start server: %s\n", err.Error())
 	}
-	//hello
+	
 }
